63748b05187e0825fd6eb9e431e0619d8406e8f8
Daniel Hair <hair4@llnl.gov>
2024-12-05 21:20:54 -0700

    chore: remove base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <base href="/treasured-bytes" />
-      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
